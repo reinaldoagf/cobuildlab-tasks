e2dd8ff411d2b0010efb4070668f32e1a5b97f97
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-07-25 03:54:33 -0400

    [edit] 'src/hooks/useTasksCustomHook.js'

--- src/hooks/useTasksCustomHook.js
+++ src/hooks/useTasksCustomHook.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-const BASE_API = `https://cobuildlab-tasks-back.herokuapp.com/api`;
+/* const BASE_API = `https://cobuildlab-tasks-back.herokuapp.com/api`; */
 const useTasksCustomHook = () => {
   const [tasks, setTasks] = useState([])
   const [loading, setLoading] = useState(false)
   useEffect(() => {
     const fetchTasks = async () => {
       setLoading(true)
-      const response = await axios(`${BASE_API}/tasks/all`)
-      setTasks(response.data.data);
+      /* const response = await axios(`${BASE_API}/tasks/all`)
+      setTasks(response.data.data); */
+      setTasks([]);
       setLoading(false)
     }
 
@@ -17,21 +18,24 @@ const useTasksCustomHook = () => {
   
   const add = async (body) => {
     setLoading(true)
-    const response = await axios.post(`${BASE_API}/tasks/store`,body)
-    setTasks([...tasks,response.data.data]);
+    /* const response = await axios.post(`${BASE_API}/tasks/store`,body)
+    setTasks([...tasks,response.data.data]); */
+    setTasks([]);
     setLoading(false)
 }
 const update = async (body) => {
   setLoading(true)
-  const response = await axios.put(`${BASE_API}/tasks/update/${body._id}`,body)
-  setTasks(tasks.map(e => e._id ===response.data.data._id ? response.data.data : e));
+  /* const response = await axios.put(`${BASE_API}/tasks/update/${body._id}`,body)
+  setTasks(tasks.map(e => e._id ===response.data.data._id ? response.data.data : e)); */
+  setTasks([]);
   setLoading(false)
 }
 
   const remove = async (element) => {
     setLoading(true)
-    const response = await axios.delete(`${BASE_API}/tasks/delete/${element._id}`)
-    setTasks(tasks.filter(e => e._id !== response.data.data._id));
+    /* const response = await axios.delete(`${BASE_API}/tasks/delete/${element._id}`)
+    setTasks(tasks.filter(e => e._id !== response.data.data._id)); */
+    setTasks([]);
     setLoading(false)
 }
 
